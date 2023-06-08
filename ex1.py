cde3340cc42d2ad7f58a2f3630c214e28c309f17
Jenaro Wachira <wjenaro@gmail.com>
2023-06-08 17:46:29 +0300

    new

---
+++ ex1.py
@@ -0,0 +1,7 @@
+class Vehicle():
+    def __init__(self, max_speed, mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+h=Vehicle(74,33)
+print(h.max_speed)
+print(h.max_speed*h.mileage)
